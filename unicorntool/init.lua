7ac9fdf9131eca5da0b78d370ea0eb6e58b2d497
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-11-15 16:57:13 -0800

    Add help messages
    Fixes #5

--- unicorntool/init.lua
+++ unicorntool/init.lua
@@ -5,11 +5,26 @@ local tArgs = {...}
 local command = tArgs[1]
 local target = tArgs[2]
 
+local usage = [[unicorntool - interface to unicornpkg
+
+Usage:
+unicorntool install /PATH/TO/FILE
+unicorntool uninstall package_name
+
+See also:
+
+help hoof
+help unicorntool
+]]
+
 if command == "install" or command == "add" then
 	local f = dofile(target)
 	unicorn.core.install(f)
 elseif command == "uninstall" or command == "remove" then
 	unicorn.core.uninstall(target)
+elseif command == "help" or command == "h" then
+	print(usage)
 else
+	print(usage)
 	error("No such command.")
 end
