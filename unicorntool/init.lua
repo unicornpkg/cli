325c906ac53d2000c3811c4b0b147db6e3c4af09
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-02-10 13:30:58 -0800

    chore: Use require with custom `package.path`
    This makes Unicornpkg able to be used inside of
    the ROM, and it is better practice than
    `dofile`ing the library.

--- unicorntool/init.lua
+++ unicorntool/init.lua
@@ -1,10 +1,19 @@
-local unicorn = dofile("/lib/unicorn/init.lua")
+package.path = "/lib/?.lua;/lib/?;/lib/?/init.lua;" .. package.path
+local unicorn = require("unicorn")
 
 local tArgs = {...}
 
 local command = tArgs[1]
 local target = tArgs[2]
 
+local function complete()
+	local completion = require "cc.shell.completion"
+		shell.setCompletionFunction(shell.getRunningProgram(), completion.build(
+ 	{ completion.choice, { "install", "uninstall" } }
+))
+end
+
+complete()
 if command == "install" or command == "add" then
 	local f = dofile(target)
 	unicorn.core.install(f)
